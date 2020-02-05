28b61402a43081e5029079e936870a3cfe887108
Vasyabester <Vasyabester@gmail.com>
2020-02-06 01:29:00 +0200

    HW26_Patterns_JS

--- src/js/article/views/LeftSideAuthorsView.js
+++ src/js/article/views/LeftSideAuthorsView.js
@@ -1,3 +1,5 @@
+import {makeBlogPostItemtEl} from '../../blog/blogPostsView';
+
 export class LeftSideAuthorsView {
   constructor(postAuthors, allPosts, mediator) {
     this.postAuthors = postAuthors;
@@ -8,15 +10,32 @@ export class LeftSideAuthorsView {
   }
 
   render() {
+    const leftSideContainerFragment = document.createDocumentFragment();
+
+    this.mainContainer = document.createElement('div');
+    this.mainContainer.className = 'blogContainer';
+
+    leftSideContainerFragment.append(this._renderButtonContainerEl(), this.mainContainer);
+
+    return leftSideContainerFragment;
+  }
+
+  _renderButtonContainerEl() {
     const buttonContainerEl = document.createElement('section');
     buttonContainerEl.className = 'articles__button-container--left';
 
     this.postAuthors.forEach((postAuthor) => {
       const postsBySelectedAuthor = this.allPosts.filter((post) => post.author === postAuthor);
-      let postListEl = '';
+      const postListEl = document.createDocumentFragment();
 
-      postsBySelectedAuthor.forEach((Post) => {
-        postListEl += `<div class="articles__post-title">${Post.title}</div>`;
+      postsBySelectedAuthor.forEach((post) => {
+        const articlePostButton = document.createElement('button');
+        articlePostButton.className = 'articles__post-title-button';
+        articlePostButton.innerHTML = post.title;
+
+        $(articlePostButton).on('click', this._openPost.bind(this, post));
+
+        postListEl.append(articlePostButton);
       });
 
       const buttonEl = document.createElement('button');
@@ -28,14 +47,12 @@ export class LeftSideAuthorsView {
 
       $(buttonEl).on('click', this._selectAuthor.bind(this));
 
-      buttonContainerEl.insertAdjacentHTML(
-        'beforeend',
-        `
-          <div class="articles__tab-post-title-container" hidden="true">
-            ${postListEl}
-          </div>
-        `,
-      );
+      const tabContainerEl = document.createElement('div');
+      tabContainerEl.className = 'articles__tab-post-title-container';
+      tabContainerEl.hidden = true;
+      tabContainerEl.append(postListEl);
+
+      buttonContainerEl.append(tabContainerEl);
     });
 
     return buttonContainerEl;
@@ -68,5 +85,32 @@ export class LeftSideAuthorsView {
       .filter((button) => button.innerText === authorName[0]);
 
     $(leftAuthorButton).trigger('click', false);
+  }
+
+  _openPost(post) {
+    const URL = `http://127.0.0.1:3000/api/articles/${post._id}`;
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('get', URL, false);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(null);
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== 4) {
+        return;
+      }
+
+      if (xhr.status !== 200) {
+        alert(JSON.parse(xhr.response).message);
+
+        return;
+      }
+    };
+
+    this._renderPost(JSON.parse(xhr.response));
+  }
+
+  _renderPost(post) {
+    this.mainContainer.innerHTML = '';
+    this.mainContainer.append(makeBlogPostItemtEl(post));
   }
 }
